662a2213d423baf41e3b9e608669d613437165a4
Lena Hou <lenahou2013@gmail.com>
2020-12-17 22:19:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,9 +3,16 @@ var evens = [0, 2, 4, 6, 8, 10];
 evens.forEach(even => {
   console.log(`${even} is not odd!`);
 }
+);
+console.log(evens.forEach(even));
 
-)
 
+var evens = [0, 2, 4, 6, 8, 10];
+
+evens.forEach(even, index, array) => {
+  debugger;
+  console.log(`${even} is not odd!`);
+});
 // Add your doToElementsInArray() function here:
 
 
